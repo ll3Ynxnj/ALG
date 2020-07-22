0e8f861bfd63defcfcae58be5199a1369d11004b
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2020-07-22 09:33:34 +0900

    Add product code 'OT3' and 'OPA'

--- run.sh
+++ run.sh
@@ -2,25 +2,25 @@
 
 export LC_CTYPE=ja_JP.UTF-8
 
-cd /Users/ll3Ynxnj/Projects/BashProjects/aikiki/
+cd ~/Projects/BashProjects/aikiki/
 
 # Clean data
 rm -f ./DataSource/*
 
 # Input data
-cp /Users/ll3ynxnj/Public/ALGIO/* ./DataSource/
+cp ~/Public/ALGIO/* ./DataSource/
 
 # Generate CSV if convertible text exist
-/Users/ll3Ynxnj/.pyenv/shims/python3 genCsvFromText.py
+~/.pyenv/shims/python3 genCsvFromText.py
 
 # Generate label data
-/Users/ll3Ynxnj/.pyenv/shims/python3 genDataFromCsv.py
+~/.pyenv/shims/python3 genDataFromCsv.py
 
 # Print label
 ./printLabels.sh
 
 # Output data
-cp ./OrderLists/* /Users/ll3ynxnj/Public/ALGIO/
+cp ./OrderLists/* ~/Public/ALGIO/
 
 # Archive data
 ./addToArchive.sh
